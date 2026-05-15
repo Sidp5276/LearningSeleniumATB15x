33e6ca6ccbb19b9a15fb2f1072074987665757c0
Siddharth <sidp5276@gmail.com>
2026-05-15 18:34:46 +0530

    init commit

---
+++ src/main/java/ex02_Selenium_Basics/TestSelenium07_Assertion_TestNG.java
@@ -0,0 +1,32 @@
+package ex02_Selenium_Basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class TestSelenium07_Assertion_TestNG {
+
+
+    @Test
+    public void test_selenium_01(){
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://google.com");
+
+        //  TestNG
+        Assert.assertEquals(driver.getTitle(),"Google");
+
+
+        // AssertJ
+        assertThat(driver.getTitle())
+                .isNotBlank()
+                .isNotEmpty()
+                .isNotNull()
+                .isEqualTo("Google");
+
+        driver.quit();
+    }
+}
